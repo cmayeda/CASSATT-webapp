b33831ee48b8b86a3c80e2c162868deab74204a2
cmayeda <cassidymayeda@gmail.com>
2023-02-16 23:48:59 -0800

    dynamic menu controls

--- CASSATT-app/www/mod_neighborhood_clickable.R
+++ CASSATT-app/www/mod_neighborhood_clickable.R
@@ -11,29 +11,36 @@ neighborhood_data = read.csv("www/neighborhood_data.csv")
 neighborhood_clickable_ui <- function(id) {
   ns <- NS(id)
   tagList(
-      fluidRow(
-          column(10, offset = 1,
-              column(4,
-                  plotOutput(ns("neighborhood"), click = ns("plot_click"))
-              ),
-              column(4, 
-                  tags$div(class = "config_menu",
-                      selectInput(ns("method"),
-                                  label = "Select a neighborhood identification method",
-                                  choices = c("voronoi", "shell", "knn"),
-                                  selected = "voronoi"),
-                      numericInput(ns("cluster_size"), 
-                                   label = "Cluster size",
-                                   value = 10, min = 3, max = 100),
-                  )
-              ),
-              column(4, 
-                  tags$p(class = "help_text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-                  nec tellus imperdiet, mollis purus non, ornare lectus. Pellentesque cursus pellentesque magna. 
-                  Etiam ac turpis bibendum, fermentum enim vitae, feugiat nulla. Morbi pharetra euismod dictum. 
-                  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
-              ) 
+    fluidRow(
+      column(10, offset = 1,
+        fluidRow(
+          column(6,
+            plotOutput(ns("neighborhood"), click = ns("plot_click"), height = "600px")
+          ),
+          column(6, 
+            tags$div(class = "config_menu",
+              selectInput(ns("method"),
+                          label = "Select a neighborhood identification method",
+                          choices = c("voronoi", "shell", "knn"),
+                          selected = "voronoi"),
+              numericInput(ns("n_neighbors"), 
+                           label = "Number of nearest neighbors",
+                           value = 10, min = 5, max = 30),
+              numericInput(ns("distance"),
+                           label = "Distance in microns",
+                           value = 10, min = 0, max = 50)
+              )
+            ),
+          ),
+          fluidRow(
+            column(8, offset = 2, 
+              tags$p(class = "help_text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
+              nec tellus imperdiet, mollis purus non, ornare lectus. Pellentesque cursus pellentesque magna. 
+              Etiam ac turpis bibendum, fermentum enim vitae, feugiat nulla. Morbi pharetra euismod dictum. 
+              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+            )        
           )
+        )
       )
   )
 }
@@ -54,6 +61,7 @@ neighborhood_clickable_server <- function(input, output, session,
       theme_clickable() 
   }, ignoreNULL = FALSE, ignoreInit = FALSE, once = TRUE)
   
+  # color plot based on red clicked cell, and blue neighbor cells 
   observe({
     np <- nearPoints(n_data, input$plot_click, maxpoints = 1, addDist = FALSE)
     np_coords = as.vector(np[1:2])
@@ -61,7 +69,6 @@ neighborhood_clickable_server <- function(input, output, session,
     if (isTruthy(np_coords[["Global_x"]])) { 
       neighbor_coords = data.frame(t(sapply(find_voronoi(np_coords), c)))
       np_plotting = data.frame(t(sapply(np_coords, c)))
-      str(np_plotting)
       
       rv$ggClickable <<- ggplot(n_data) +
         coord_fixed() +
@@ -71,20 +78,21 @@ neighborhood_clickable_server <- function(input, output, session,
         geom_point(np_plotting, mapping = aes(x = Global_x, y = Global_y), cex = dot_size, col = "red") + 
         theme_clickable()
     } 
-    
-    # if (nrow(np) <= 0) { 
-
-    # } else {
-    #   rv$ggClickable <<- ggplot(tSNE_plot()) +
-    #     coord_fixed() +
-    #     geom_point(aes(x = x, y = y, color = fSOM_clusters()), cex = 3) +
-    #     scale_color_manual(values = my_magma()) +
-    #     geom_point(cluster_set, mapping = aes(x = x, y = y),
-    #                fill = fill_color, cex = 3, shape = 21, color = "#005CCB") +
-    #     theme_clickable()
-    # }
   })
   
+  # hide and show controls based on neighbor ID method
+  observe({
+    if (input$method == "knn") {
+      showElement("n_neighbors")
+    } else {
+      hideElement("n_neighbors")
+    }
+    if (input$method == "shell") {
+      showElement("distance")
+    } else {
+      hideElement("distance")
+    }
+  })
 
   
   output$neighborhood <- renderPlot({
