02b1333c30c2c2659e4881fa62201801123a3ee7
cmayeda <cassidymayeda@gmail.com>
2023-06-21 16:16:51 -0700

    fix giraph selection after plot data reorder

--- CASSATT-app/www/mod_pop_clickable.R
+++ CASSATT-app/www/mod_pop_clickable.R
@@ -50,11 +50,25 @@ pop_clickable_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     
     rv <- reactiveValues(current_pop = NULL,
-                         ordered_data = neighborhood_data)
+                         ordered_data = neighborhood_data,
+                         gir = NULL)
 
-    # create interactive plot and legend 
-    gir <- reactive({
-      ggplot(rv$ordered_data) +
+    gir_options = list(
+      opts_toolbar(saveaspng = FALSE),
+      opts_hover(css = "stroke:#cece5b2; stroke-width:1px;"),
+      opts_hover_key(css = "stroke:#cece5b2; stroke-width:1px;"),
+      opts_selection(css = "stroke:#000000; stroke-width:1px;", type = "single"),
+      opts_selection_key(css = "stroke:#000000; stroke-width:1px;", type = "single")
+    )
+    
+    # set initial plot data order 
+    observeEvent( input$plot_selected, {
+      rv$ordered_data <<- neighborhood_data
+    }, ignoreInit = FALSE, ignoreNULL = FALSE, once = TRUE)
+    
+    # set up interactive plot and legend 
+    output$plot <- renderGirafe({ 
+      gg = ggplot(rv$ordered_data) +
         geom_point_interactive(aes(
           x = Global_x, y = Global_y,
           col = pop_ID, tooltip = pop_ID, data_id = pop_ID
@@ -71,48 +85,32 @@ pop_clickable_server <- function(id) {
         coord_fixed() +
         scale_y_reverse() +
         theme_clickable()
+      girafe(ggobj = gg, options = gir_options)
     })
     
-    # place clicked population on top   
-    observeEvent( rv$current_pop, {
-      indxs = which(neighborhood_data$pop_ID == rv$current_pop)
+    # reorder plot data with clicked population on top 
+    selected = NULL
+    
+    observeEvent( input$plot_key_selected, {
+      selected <<- input$plot_key_selected
+      indxs = which(neighborhood_data$pop_ID == input$plot_key_selected)
       step = neighborhood_data[-indxs, ]
       rv$ordered_data <<- rbind(step, neighborhood_data[indxs, ])
-    }, ignoreInit = TRUE, ignoreNULL = TRUE)
+    }, ignoreInit = TRUE)
     
-    # set plot to match legend click 
-    observeEvent( input$plot_key_selected, {
-      if (is.null(input$plot_key_selected)) {
-        rv$current_pop <<- NULL
-        session$sendCustomMessage(type = "pop_clickable-plot_set", message = character(0))
-      } else {
-        rv$current_pop <<- input$plot_key_selected
-        session$sendCustomMessage(type = "pop_clickable-plot_set", rv$current_pop)
-      }
-    }, ignoreInit = TRUE, ignoreNULL = FALSE)
-    
-    # set legend to match plot click 
     observeEvent( input$plot_selected, {
-      if (is.null(input$plot_selected)) {
-        rv$current_pop <<- NULL
-        session$sendCustomMessage(type = "pop_clickable-plot_key_set", message = character(0))
-      } else {
-        rv$current_pop <<- input$plot_selected
-        session$sendCustomMessage(type = "pop_clickable-plot_key_set", message = rv$current_pop)
-      }
-    }, ignoreInit = TRUE, ignoreNULL = FALSE)
+      selected <<- input$plot_selected
+      indxs = which(neighborhood_data$pop_ID == input$plot_selected)
+      step = neighborhood_data[-indxs, ]
+      rv$ordered_data <<- rbind(step, neighborhood_data[indxs, ])
+    }, ignoreInit = TRUE)
     
-    gir_options = list(
-      opts_toolbar(saveaspng = FALSE),
-      opts_hover(css = "stroke:#cece5b2; stroke-width:1px;"),
-      opts_hover_key(css = "stroke:#cece5b2; stroke-width:1px;"),
-      opts_selection(css = "stroke:#000000; stroke-width:1px;", type = "single"),
-      opts_selection_key(css = "stroke:#000000; stroke-width:1px;", type = "single")
-    )
+    # after reordering plot, re-select clicked population 
+    session$onFlushed(function() {
+      session$sendCustomMessage(type = "pop_clickable-plot_set", message = selected)
+      session$sendCustomMessage(type = "pop_clickable-plot_key_set", message = selected)
+    }, once = FALSE)
     
-    output$plot <- renderGirafe({
-      girafe(ggobj = gir(), options = gir_options)
-    })
   })
 }
 
