934d79262412374aeb0892e70e78860885a42f84
cmayeda <cassidymayeda@gmail.com>
2023-06-26 12:39:11 -0700

    change pop click highlight color to yellow

--- CASSATT-app/www/mod_pop_clickable.R
+++ CASSATT-app/www/mod_pop_clickable.R
@@ -16,6 +16,7 @@ library(ggiraph)
 source("www/custom_themes_palettes.R")
 
 neighborhood_data = read.csv("www/neighborhood_data.csv")
+neighborhood_data$kmeans_cluster <- as.factor(neighborhood_data$kmeans_cluster)
 
 pop_clickable_ui <- function(id) {
   ns <- NS(id)
@@ -30,7 +31,7 @@ pop_clickable_ui <- function(id) {
               tags$div(class = "config_menu",
                   selectInput(ns("method"),
                       label = "Method of population identification",
-                      choices = c("expert gating", "automatic clustering"),
+                      choices = c("expert gating", "kmeans clustering"),
                       selected = "expert gating"),
               ),
               tags$p(class = "help_text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
@@ -51,22 +52,22 @@ pop_clickable_server <- function(id) {
     
     rv <- reactiveValues(ordered_data = neighborhood_data)
 
-    outline_click = "#e3d2c3"
-    outline_hover = "#a79484"
+    outline_click = "#fbb700"
+    outline_hover = "#ddcca1"
     text_click = "#000000"
     text_hover = "#515151"
     
     gir_options = list(
       opts_toolbar(saveaspng = FALSE),
-      opts_hover(css = paste0("stroke:",outline_hover,"; stroke-width:1.6px;")),
+      opts_hover(css = paste0("stroke:",outline_hover,"; stroke-width:1px;")),
       opts_hover_key(css = girafe_css(
-        css = paste0("stroke:",outline_hover,"; stroke-width:1.6px;"),
-        text = paste0("stroke:",text_hover,"; stroke-width:0.4px;")
+        css = paste0("stroke:",outline_hover,"; stroke-width:1px;"),
+        text = paste0("stroke:",text_hover,"; stroke-width:0.5px;")
       )),
-      opts_selection(css = paste0("stroke:",outline_click,"; stroke-width:1.6px;"), type = "single"),
+      opts_selection(css = paste0("stroke:",outline_click,"; stroke-width:1px;"), type = "single"),
       opts_selection_key(css = girafe_css(
-        css = paste0("stroke:",outline_click,"; stroke-width:1.6px"),
-        text = paste0("stroke-width:0.4px; stroke:",text_click,";")
+        css = paste0("stroke:",outline_click,"; stroke-width:1px"),
+        text = paste0("stroke-width:0.5px; stroke:",text_click,";")
       ), type = "single")
     )
     
@@ -77,15 +78,26 @@ pop_clickable_server <- function(id) {
     
     # interactive plot and legend 
     output$plot <- renderGirafe({ 
+      col = "pop_ID"
+      pal = summertime_pal
+      if (input$method == "kmeans clustering") { 
+        col <- "kmeans_cluster"
+        set.seed(3)
+        pal <- sample(summertime_expanded)
+        names(pal) <- as.character(1:15)
+      }
+      
       gg = ggplot(rv$ordered_data) +
         geom_point_interactive(aes(
           x = Global_x, y = Global_y,
-          col = pop_ID, tooltip = pop_ID, data_id = pop_ID
+          col = rv$ordered_data[, col], 
+          tooltip = rv$ordered_data[, col], 
+          data_id = rv$ordered_data[, col]
         ), cex = dot_size) +
         scale_color_manual_interactive(
           name = "Population",
-          values = summertime_pal,
-          breaks = names(summertime_pal),
+          values = pal,
+          breaks = names(pal),
           data_id = function(breaks) { breaks },
           labels = function(breaks) { lapply(breaks, function(br) {
             label_interactive(br, data_id = br)
@@ -101,9 +113,13 @@ pop_clickable_server <- function(id) {
     selected = NULL
     
     observeEvent( input$plot_key_selected, {
+      col = "pop_ID"
+      if (input$method == "kmeans clustering") { 
+        col = "kmeans_cluster"
+      }
       if (!is.null(input$plot_key_selected)) {
         selected <<- input$plot_key_selected
-        indxs = which(neighborhood_data$pop_ID == input$plot_key_selected)
+        indxs = which(neighborhood_data[, col] == selected)
         step = neighborhood_data[-indxs, ]
         rv$ordered_data <<- rbind(step, neighborhood_data[indxs, ])
       } else {
@@ -113,9 +129,13 @@ pop_clickable_server <- function(id) {
     }, ignoreInit = TRUE, ignoreNULL = FALSE)
     
     observeEvent( input$plot_selected, {
+      col = "pop_ID"
+      if (input$method == "kmeans clustering") { 
+        col = "kmeans_cluster"
+      }
       if (!is.null(input$plot_selected)) {
         selected <<- input$plot_selected
-        indxs = which(neighborhood_data$pop_ID == input$plot_selected)
+        indxs = which(neighborhood_data[, col] == selected)
         step = neighborhood_data[-indxs, ]
         rv$ordered_data <<- rbind(step, neighborhood_data[indxs, ])
       } else {
