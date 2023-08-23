261ba43aa7b2ed524443de20ba56cb6531a258d1
cmayeda <cassidymayeda@gmail.com>
2023-08-23 15:14:29 -0700

    reimplement decagons

--- CASSATT-app/www/mod_neighborhood_clickable.R
+++ CASSATT-app/www/mod_neighborhood_clickable.R
@@ -107,6 +107,7 @@ neighborhood_clickable_server <- function(input, output, session, server_rv) {
   
   # hide and show controls based on neighbor ID method
   observeEvent( input$method, {
+    RUN_NEEDED <<- TRUE
     if (input$method == "knn") {
       updateNumericInput(session, "num", label = "Number of nearest neighbors", value = 10, min = 5, max = 30)
       updateActionButton(session, "run", label = "Calculate nearest neighbors")
@@ -132,7 +133,6 @@ neighborhood_clickable_server <- function(input, output, session, server_rv) {
     selected <<- character(0)
     rv$selected_point <<- empty_row
     rv$selected_neighbors <<- empty_row
-    # rv$d_colors <<- as.list(rep("#ffffff", 10))
   })
   
   # set up warnings 
@@ -187,7 +187,6 @@ neighborhood_clickable_server <- function(input, output, session, server_rv) {
         orig_indx = as.numeric(input$plot_selected)
         c = neighborhood_data[orig_indx, c("Global_x","Global_y")]
         rv$selected_point <<- cbind(c, orig_indx)
-        
         neighbors = data.frame()
         if (input$method == "voronoi") {
           neighbors <- find_voronoi(rv$selected_point)
@@ -198,12 +197,16 @@ neighborhood_clickable_server <- function(input, output, session, server_rv) {
         }
         
         # retrieve r indx from py attribute
-        indx_att = attributes(neighbors)[["pandas.index"]]
-        r_indx = c()
-        for (i in 1:length(indx_att)) {
-          r_indx[i] <- as.numeric(paste(indx_att[i - 1])) + 1
+        if (nrow(neighbors) > 0) {
+          indx_att = attributes(neighbors)[["pandas.index"]]
+          r_indx = c()
+          for (i in 1:length(indx_att)) {
+            r_indx[i] <- as.numeric(paste(indx_att[i - 1])) + 1
+          }
+          rv$selected_neighbors <<- cbind(neighbors, orig_indx = r_indx)
+        } else {
+          rv$selected_neighbors <<- empty_row
         }
-        rv$selected_neighbors <<- cbind(neighbors, orig_indx = r_indx)
       }
     }
   }, ignoreInit = T, ignoreNULL = F)
@@ -225,52 +228,52 @@ neighborhood_clickable_server <- function(input, output, session, server_rv) {
   }, ignoreInit = T) 
   
   # plot decagons of cluster cell types 
-  # observeEvent(rv$selected_neighbors, {
-  #   if (nrow(rv$selected_neighbors) >= 1) {
-  #     rv$d_colors <<- deca_colors(rv$selected_neighbors, server_rv$colormode)
-  #   } else {
-  #     rv$d_colors <<- as.list(rep("#ffffff", 10))
-  #   }
-  # }, ignoreInit = T)
+  observeEvent(rv$selected_neighbors, {
+    if (nrow(rv$selected_neighbors) >= 1) {
+      rv$d_colors <<- deca_colors(rv$selected_neighbors, server_rv$colormode)
+    } else {
+      rv$d_colors <<- as.list(rep("#ffffff", 10))
+    }
+  }, ignoreInit = T)
   
-  # output$decagons <- renderPlot({
-  #   plot = ggplot(decagons)
-  #   for (i in 1:10) {
-  #     indxes = which(decagons$name == as.character(i))
-  #     plot <- plot + geom_polygon(
-  #       data = decagons[indxes, ], aes(x = x, y = y),
-  #       fill = rv$d_colors[[i]],
-  #       color = "black", linewidth = 1.25
-  #     )
-  #   }
-  #   plot <- plot + 
-  #     coord_fixed() + 
-  #     theme_deca()
-  #   return(plot)
-  # })
+  output$decagons <- renderPlot({
+    plot = ggplot(decagons)
+    for (i in 1:10) {
+      indxes = which(decagons$name == as.character(i))
+      plot <- plot + geom_polygon(
+        data = decagons[indxes, ], aes(x = x, y = y),
+        fill = rv$d_colors[[i]],
+        color = "black", linewidth = 1.25
+      )
+    }
+    plot <- plot +
+      coord_fixed() +
+      theme_deca()
+    return(plot)
+  })
   
-  # observeEvent( rv$d_colors, {
-  #   colors = unique(unlist(rv$d_colors))
-  #   if (server_rv$colormode == "custom") { 
-  #     types = sapply(colors, function(x) { as.numeric(which(summertime_pal == x)) })
-  #     types <- names(summertime_pal)[types]
-  #   } else {
-  #     types = sapply(colors, function(x) { as.numeric(which(viridis_expert == x)) })
-  #     types <- names(viridis_expert)[types]
-  #   }
-  #   df = data.frame(x = c(1:length(colors)))
-  #   plot = ggplot(df, aes(x = x, y = 1, col = as.factor(x))) +
-  #     geom_point() +
-  #     scale_color_manual(values = colors, labels = types, name = "Population") +
-  #     theme_bw() +
-  #     theme(
-  #       legend.title = element_text(size = 14.5, lineheight = 1.3),
-  #       legend.text = element_text(size = 14.5, lineheight = 1.3),
-  #       legend.justification = "left"
-  #     )
-  #   rv$deca_key <<- get_legend(plot)
-  # }, ignoreInit = FALSE)
-  
+  observeEvent( rv$d_colors, {
+    colors = unique(unlist(rv$d_colors))
+    if (server_rv$colormode == "custom") {
+      types = sapply(colors, function(x) { as.numeric(which(summertime_pal == x)) })
+      types <- names(summertime_pal)[types]
+    } else {
+      types = sapply(colors, function(x) { as.numeric(which(viridis_expert == x)) })
+      types <- names(viridis_expert)[types]
+    }
+    df = data.frame(x = c(1:length(colors)))
+    plot = ggplot(df, aes(x = x, y = 1, col = as.factor(x))) +
+      geom_point() +
+      scale_color_manual(values = colors, labels = types, name = "Population") +
+      theme_bw() +
+      theme(
+        legend.title = element_text(size = 14.5, lineheight = 1.3),
+        legend.text = element_text(size = 14.5, lineheight = 1.3),
+        legend.justification = "left"
+      )
+    rv$deca_key <<- get_legend(plot)
+  }, ignoreInit = FALSE)
+
   output$deca_key <- renderPlot({ grid.draw(rv$deca_key) })
   
 }
