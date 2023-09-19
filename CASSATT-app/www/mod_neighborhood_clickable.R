edfa5ea2ca6bdb7cd1b8241e0da4840263d8c4ab
Asa <asasama12@gmail.com>
2023-09-19 09:45:31 -0500

    Drafted help text throughout

--- CASSATT-app/www/mod_neighborhood_clickable.R
+++ CASSATT-app/www/mod_neighborhood_clickable.R
@@ -20,9 +20,10 @@ neighborhood_clickable_ui <- function(id) {
         )
       ),
       column(4,
-        tags$p(class = "help_text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-        nec tellus imperdiet, mollis purus non, ornare lectus. Pellentesque cursus pellentesque magna. 
-        Etiam ac turpis bibendum, fermentum enim vitae, feugiat nulla."), 
+        tags$p(class = "help_text", "Cell neighbors can be identified by three methods: Voronoi neighbors are the collection of cells that are 
+        the nearest seed point in the voronoi diagram in every direction from the index cell. Shell neighbors are any cell whose coordinates 
+               fall within a distance set by the user. k-nearest neighbors are the user-defined number of neighbors that are nearest in distance to any index cell. Try each
+               identification method and click on cells to see which neighbor cells are analyzed! Neighborhood characteristic plots for selected cell are shown below."), 
         tags$div(class = "config_menu",
           selectInput(ns("method"), label = "Select an identification method",
                       choices = c("voronoi", "shell", "knn"), selected = "voronoi"),
